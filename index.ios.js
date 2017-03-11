722337a249b08020a16cea42968781ada58fd5d4
Brian Best <brian.brianb64@gmail.com>
2017-03-11 16:47:37 -0400

    Feat: Locations now list with monster totals
    All the locations are showing up now with the total count of how many
    monsters are in that area

--- index.ios.js
+++ index.ios.js
@@ -15,6 +15,8 @@ import {
   ScrollView,
 } from 'react-native'
 
+import {monsters} from './monsters';
+
 export class ListItem extends Component {
   render (){
     return (
@@ -32,11 +34,17 @@ export default class ffxmonster extends Component {
     this.state = {text:''};
   }
   render() {
-    
+    let LocationItems = monsters.map((location)=>{
+      let monsterTotal = location.monsters.length;
+      let caught = 0;
+      return (
+        <ListItem key={location.name} name={location.name} collected={caught} total={monsterTotal}></ListItem>
+      )
+    });
     return (
       <ScrollView 
         horizontal={false} style={{marginTop:20}}>
-        <ListItem name="Besaid" collected={1} total={10}></ListItem>
+        {LocationItems}
       </ScrollView>
       
     );
@@ -47,181 +55,6 @@ const styles = StyleSheet.create({
   
 });
 
-const monsters = [
-  {name:"Besaid",
-   monsters: [
-     {name:"Dingo"},
-     {name:"Condor"},
-     {name:"Water Flan"},
-   ]
-  },
-  {name:"Kilika",
-    monsters: [
-     {name:"Dinonix"},
-     {name:"Killer Bee"},
-     {name:"Yellow Element"},
-     {name:"Ragora"},
-   ]
-  },
-  {name:"Mi'ihen Highroad",
-   monsters: [
-     {name:"Mi'ihen Fang"},
-     {name:"Ipiria"},
-     {name:"Floating Eye"},
-     {name:"White Element"},
-     {name:"Raldo"},
-     {name:"Vouivre"},
-     {name:"Bomb"},
-     {name:"Dual Horn"},
-   ]
-  },
-  {name:"Mushroom Rock Road",
-   monsters: [
-     {name:"Raptor"},
-     {name:"Gandarewa"},
-     {name:"Thunder Flan"},
-     {name:"Red Element"},
-     {name:"Lamashtu"},
-     {name:"Funguar"},
-     {name:"Garuda"},
-   ]
-  },
-  {name:"Djose",
-  monsters: [
-     {name:"Garm"},
-     {name:"Simurgh"},
-     {name:"Bite Bug"},
-     {name:"Snow Flan"},
-     {name:"Bunyip"},
-     {name:"Basilisk"},
-     {name:"Ochu"},
-   ]
-  },
-  {name:"Thunder Plains",
-  monsters: [
-     {name:"Melusine"},
-     {name:"Aerouge"},
-     {name:"Buer"},
-     {name:"Gold Element"},
-     {name:"Kusiraqqu"},
-     {name:"Larva"},
-     {name:"Iron Giant"},
-     {name:"Quactuar"},
-   ]
-  },
-  {name:"Macalania",
-  monsters: [
-     {name:"Snow Wolf"},
-     {name:"Iguion"},
-     {name:"Wasp"},
-     {name:"Evil Eye"},
-     {name:"Ice Flan"},
-     {name:"Blue Element"},
-     {name:"Murussu"},
-     {name:"Mafdet"},
-     {name:"Xiphos"},
-     {name:"Chimera"},
-   ]},
-  {name:"Bikanel",
-  monsters: [
-     {name:"Sand Wolf"},
-     {name:"Alcyone"},
-     {name:"Mushusuu"},
-     {name:"Zu"},
-     {name:"Sand Worm"},
-     {name:"Cactuar"},
-   ]},
-  {name:"Calm Lands",
-  monsters: [
-     {name:"Skoll"},
-     {name:"Nebiros"},
-     {name:"Flame Flan"},
-     {name:"Shred"},
-     {name:"Anacondaur"},
-     {name:"Ogre"},
-     {name:"Coeurl"},
-     {name:"Chimera Brain"},
-     {name:"Malboro"},
-   ]},
-  {name:"Sunken Cave",
-  monsters: [
-     {name:"Yowie"},
-     {name:"Imp"},
-     {name:"Dark Element"},
-     {name:"Nidhogg"},
-     {name:"Thorn"},
-     {name:"Valaha"},
-     {name:"Epaaj"},
-     {name:"Ghost"},
-     {name:"Tonberry"},
-   ]},
-  {name:"Gagazet",
-  monsters: [
-     {name:"Bandersnatch"},
-     {name:"Grat"},
-     {name:"Bashura"},
-     {name:"Grenade"},
-     {name:"Splasher"},
-     {name:"Achelous"},
-     {name:"Maelspike"},
-     {name:"Ahriman"},
-     {name:"Grendel"},
-    {name:"Mandragora"},
-    {name:"Behemoth"},
-    {name:"Dark Flan"},
-   ]},
-  {name:"Sin",
-  monsters: [
-     {name:"Exoray"},
-     {name:"Wraith"},
-     {name:"Gemini A"},
-     {name:"Gemini B"},
-     {name:"Demonolith"},
-     {name:"Great Malboro"},
-     {name:"Barbatos"},
-     {name:"Adamantoise"},
-     {name:"Behemoth King"},
-   ]},
-  {name:"Omega Dungeon",
-  monsters: [
-     {name:"Zaurus"},
-     {name:"Floating Death"},
-     {name:"Black Element"},
-     {name:"Halma"},
-     {name:"Puroboros"},
-     {name:"Spirit"},
-     {name:"Maches"},
-     {name:"Master Coeurl"},
-     {name:"Master Tonberry"},
-    {name:"Varuna"},
-   ]},
-  {name:"Original Monsters",
-  monsters: [
-     {name:"Stratoavis"},
-    {name:"Malboro Menace"}, 
-    {name:"Kottos"},
-    {name:"Couerlregina"},
-    {name:"Jormungand"},
-    {name:"Cactuar King"}, 
-    {name:"Espada"},
-    {name:"Abyss Worm"},
-    {name:"Chimerageist"},
-    {name:"Don Tonberry"},
-    {name:"Catoblepas"},
-    {name:"Abaddon"},
-    {name:"Vorban"},
-     {name:"Greater Sphere"},
-     {name:"Th'uban"},
-     {name:"Earth Eater"},
-     {name:"Catastrophe"},
-     {name:"Shinryu"},
-     {name:"Neslug"},
-     {name:"Ultima Buster"},
-     {name:"Nemesis"},
-     {name:"Master Tonberry"},
-    {name:"Varuna"},
-   ]},
-  
-];
+
 
 AppRegistry.registerComponent('ffxmonster', () => ffxmonster);
